ba14d5e6f75706404455afc538ec43343faf8f8d
Anderson <andersoncmp1@gmail.com>
2026-07-21 16:31:41 -0300

    chore: trigger Vercel deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// Trigger Vercel deployment update
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import { steps } from './data/quiz';
